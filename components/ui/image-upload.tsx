8b685cb2c67a8863b6f93871c43c54f63be02cfe
Gareth Grindeland <gt.grindeland@gmail.com>
2023-07-27 09:11:47 -0700

    update cloudinary uploadPreset in ui/image-upload.tsx

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="lcyxjy9a">
         {({ open }) => {
           const onClick = () => {
             open();
